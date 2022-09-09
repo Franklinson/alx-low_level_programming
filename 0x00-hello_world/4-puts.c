7a537e9368152ee3c3651e7637655df83ac78a34
Franklinson <franklinhassey@gmail.com>
2022-09-09 13:31:28 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-/*Trial code for c programming*/
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
